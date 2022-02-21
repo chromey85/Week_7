5a89b5a102b4012f9f03581fa70104774b79c685
chromey85 <yayyub85@gmail.com>
2022-02-21 18:03:39 +0000

    array in yargs

--- Yargs/src/app.js
+++ Yargs/src/app.js
@@ -7,9 +7,17 @@ const app = (yargsObj) => {
         movie.add();
         console.log(movie.list());
         // add movie to array, consol log at end of runtime
-    } else {
-        console.log("Incorrect Command")
-    }
+    } else if (yargsObj.addMore) {
+            const movie = new Movie(yargsObj.title, yargsObj.actor);
+            const movie1 = new Movie(yargsObj.title1, yargsObj.actor1);
+            const movie2 = new Movie(yargsObj.title2, yargsObj.actor2);
+            const movie3 = new Movie(yargsObj.title3, yargsObj.actor3);
+            movie.add();
+            movie1.add()
+            console.log(movie1.list());
+        } else {
+            console.log("Incorrect Command")
+        }
 };
 
 app(yargs.argv);
