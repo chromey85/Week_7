3a910eae311ff958d5510a428d6fd04fa297c721
chromey85 <yayyub85@gmail.com>
2022-02-23 00:17:42 +0000

    add e.g data and added update

--- Node-cli_Intro/src/app.js
+++ Node-cli_Intro/src/app.js
@@ -3,7 +3,9 @@ const Movie = require("./utils");
 const app = (args) => {
     switch (args[2]) {
         case "add":
+            console.log(args)
             const movie = new Movie(args[3], args[4]);
+            console.log(movie.add())
 
         case "addMulti":
             const movie1 = new Movie(args[3], args[4])
@@ -38,6 +40,38 @@ app(process.argv)
 //         default:
 //             console.log("Incorrect input")
 //             break;
+//     }
+// };
+
+// app(process.argv)
+
+// This is code provided by someone on another server
+
+
+// const movies = [];
+
+// function addMovie(title, actor) {
+//     movies.push(new Movie(title, actor));
+// }
+
+// const app = (args) => {
+//     if (args.length < 3) {
+//         console.log("Incorrect input");
+//         // maybe show the user how to use this program
+//         return -1;
+//     }
+
+//     for (let i = 0; i < args.length; i++) {
+//         switch (args[i]) {
+//             case "add":
+//                 addMovie(args[++i], args[++i]); // ++i skips the next argument
+//                 break;
+//         }
+//     }
+
+//     // print movies
+//     for (let i = 0; i < movies.length; i++) {
+//         console.log(movies[i].title + ": " + movies[i].actor);
 //     }
 // };
 
